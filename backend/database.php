633d9ea933ec04a6d338ab60d326a2489a8bc536
konradxmalinowski <malinowski.konrad45@gmail.com>
2025-06-11 18:42:50 +0200

    updated database data

--- backend/database.php
+++ backend/database.php
@@ -14,7 +14,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 define('DB_HOST', 'mysql.ct8.pl');
 define('DB_NAME', 'm42958_shop');
 define('DB_USER', 'm42958_admin');
-define('DB_PASSWORD', 'Adminadminadmin123');
+define('DB_PASS', 'Adminadminadmin123');
 
 
 
